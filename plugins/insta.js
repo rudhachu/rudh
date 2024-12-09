1d96b57badde0e5ce24ef400a631a645f3d702ce
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-12-10 03:56:49 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,28 +1,42 @@
-const fetch = require('node-fetch'); 
-const { rudhra, mode, getJson } = require("../lib");
-const axios = require('axios');
+const {
+    rudhra,
+    getJson,
+    mode,
+    getBuffer
+} = require("../lib/");
+const fetch = require('node-fetch');
 
 rudhra({
     pattern: 'insta ?(.*)',
     fromMe: mode,
     desc: 'Download Instagram Reels',
-    type: 'info'
+    type: 'Downloader'
 }, async (message, match, client) => {
-    const url = match || message.reply_message.text;
-    const apiUrl = `https://combative-sarine-eypz-god-d4cce0fc.koyeb.app/igdl?url=${url}`;
-    
+    const instaUrl = match || message.reply_message.text;
+
+    if (!instaUrl) {
+        return await message.reply('_Enter an Instagram URL!_');
+    }
+
     try {
-        const response = await axios.get(apiUrl);
-        if (response.data && response.data.medias && response.data.medias.length > 0) {
-            const mediaLimit = Math.min(response.data.medias.length, 10);
-            for (let i = 0; i < mediaLimit; i++) {
-                await message.sendFile(response.data.medias[i]);
-            }
-        } else {
-            await message.reply('No media found for the provided Instagram URL.');
+        let resi = await getJson(`https://api.devstackx.in/v1/igdl?url=${instaUrl}`);
+        
+        if (!resi || !resi.data || resi.data.length === 0) {
+            return await message.reply('_No media found or invalid URL!_');
+        }
+
+        await message.sendMessage(message.jid, "_Uploading..._", { quoted: message.data });
+
+        for (let media of resi.data) {
+            await message.client.sendMessage(
+                message.jid,
+                { video: { url: media.url }, // Ensure proper format for sending media
+                mimetype: "video/mp4"},
+                { quoted: message.data }
+            );
         }
     } catch (error) {
-        console.error('Error fetching Instagram media:', error);
-        await message.reply('An error occurred while fetching the media. Please try again later.');
+        console.error('Error fetching media:', error);
+        await message.reply('_Error fetching media!_');
     }
 });
