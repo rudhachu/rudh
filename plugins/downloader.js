8b6b99a94a7d179611432b02e3c953a1d0a30e71
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-02-26 09:54:52 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 rudhra({
-    pattern: 'fb ?(.*)',
+    pattern: 'fbd ?(.*)',
     fromMe: mode,
     desc: 'Download Facebook Videos',
     type: 'downloader'
@@ -76,5 +76,45 @@ rudhra({
     } catch (error) {
         console.error(error);
         await client.sendMessage(message.jid, { text: "An error occurred while fetching the media. Please try again later." });
+    }
+});
+
+
+rudhra({
+    pattern: 'fb ?(.*)',
+    fromMe: mode,
+    desc: 'Download facebook videos.',
+    type: 'downloader',
+}, async (message, match, client) => {
+    try {
+        const url = match || message.reply_message.text;
+        if (!url) {
+            return await message.reply("Please provide a valid Instagram URL.");
+        }
+
+        const fbApi = `https://api.siputzx.my.id/api/d/igdl?url=${url}`;
+        const res = await fetch(fbApi);
+        if (!res.ok) {
+            return await message.reply("Please try again.");
+        }
+        await message.reply('_Uploading media...⎙_', { quoted: message.data });
+        
+        const data = await res.json();
+        const igmedia = data.data;
+
+        if (igmedia && igmedia.length > 0) {
+            let counter = 0;
+            for (const media of igmedia) {
+                if (counter >= 10) break;
+                const mediaurl = media.url;
+                await message.sendFile(mediaurl);
+                counter++;
+            }
+        } else {
+            await message.reply("No media found for the provided URL.");
+        }
+    } catch (error) {
+        console.error(error);
+        await message.reply(" 'error' ");
     }
 });
