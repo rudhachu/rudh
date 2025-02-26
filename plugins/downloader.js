7002894f2ce7bda2a54d1fc9bdc01ce98889c94d
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-02-26 09:56:10 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -118,3 +118,44 @@ rudhra({
         await message.reply(" 'error' ");
     }
 });
+
+
+
+rudhra({
+    pattern: 'story ?(.*)',
+    fromMe: mode,
+    desc: 'Download Instagram Story.',
+    type: 'downloader',
+}, async (message, match, client) => {
+    try {
+        const url = match || message.reply_message.text;
+        if (!url) {
+            return await message.reply("Please provide a valid Instagram URL.");
+        }
+
+        const instaApi = `https://api.siputzx.my.id/api/d/igdl?url=${url}`;
+        const res = await fetch(instaApi);
+        if (!res.ok) {
+            return await message.reply("Please try again.");
+        }
+        await message.reply('_Uploading media...⎙_', { quoted: message.data });
+        
+        const data = await res.json();
+        const igmedia = data.data;
+
+        if (igmedia && igmedia.length > 0) {
+            let counter = 0;
+            for (const media of igmedia) {
+                if (counter >= 10) break;
+                const mediaurl = media.url;
+                await message.sendFile(mediaurl);
+                counter++;
+            }
+        } else {
+            await message.reply("No media found for the provided URL.");
+        }
+    } catch (error) {
+        console.error(error);
+        await message.reply(" 'error' ");
+    }
+});
