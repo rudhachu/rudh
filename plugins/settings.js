5a9fac42c3de2226cf5c559330751c8f2339147d
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-24 23:42:20 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -11,35 +11,7 @@ rudhra({
 await message.send("_rebooting_");
 return require('pm2').restart('index.js');
 });
-const value = ["99000","88000","77000","660000","55000","44000","33000","22000","11000","9999","999","99"];
-rudhra({
-    pattern: "sc",
-    fromMe: mode,
-    desc: "Rudhra",
-    type: "info",
-}, async (message, match, client) => {
-    var amount = value[Math.floor(Math.random() * value.length)];
-    const amountInPaise = parseInt(amount, 10) * 1000;
-    const cap = "ʀᴜᴅʜʀᴀ-ʙᴏᴛ\n\nRᴇᴘᴏ:*https://github.com/princerudh/rudhra-bot*";
 
-    await message.client.relayMessage(message.jid, {
-        requestPaymentMessage: {
-            currencyCodeIso4217: 'INR',
-            amount1000: amountInPaise,
-            requestFrom: message.sender,
-            noteMessage: {
-                extendedTextMessage: {
-                    text: cap,
-                    contextInfo: {
-                        externalAdReply: {
-                            showAdAttribution: true
-                        }
-                    }
-                }
-            }
-        }
-    }, { quoted: message.quoted });
-});
 rudhra({
   pattern: 'plugin ?(.*)',
   fromMe: true,
