22ee829f616d29e196781a8625be9ad9949496b6
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-01 04:07:20 +0530

    Update banbot.js

--- plugins/banbot.js
+++ plugins/banbot.js
@@ -1,13 +1,11 @@
 const { rudhra,PREFIX,updateBan, getBanStatus } = require("../lib");
 const config = require("../config");
-rudhra(
-  {
+rudhra({
     pattern: "bot ?(.*)",
     fromMe: true,
     desc: "bot on or off in specific group",
     type: "user",
-  },
-  async (message, match) => {
+},async (message, match) => {
     const status = await getBanStatus(message.jid);
     let link = `${config.BOT_INFO.split(";")[2]}`;
     let url = await message.ParseButtonMedia(link);
@@ -18,25 +16,25 @@ rudhra(
         {
           type: "reply",
           params: {
-            display_text: "ON",
+            display_text: "ᴏɴ",
             id: `${PREFIX}bot on`,
           },
         },
         {
           type: "reply",
           params: {
-            display_text: "OFF",
+            display_text: "ᴏꜰꜰ",
             id: `${PREFIX}bot off`,
           },
         },
       ],
       header: {
-        title: `${config.BOT_NAME}`,
+        title: `𝗥𝗨𝗗𝗛𝗥𝗔 𝗕𝗢𝗧`,
         subtitle: "",
         hasMediaAttachment: true,
       },
       footer: {
-        text: "STATUS: " + status,
+        text: "ᴄᴜʀʀᴇɴᴛ ꜱᴛᴀᴛᴜꜱ : " + status,
       },
       body: {
         text: "",
